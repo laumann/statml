fb5acfb614feecad946fa0c89bd6e4f8f6882be2
Thomas Bracht Laumann Jespersen <laumann.thomas@gmail.com>
2012-02-29 19:12:07 +0100

    Print the error rates.
    When using sprintf in MATLAB it doesn't automatically print the result.
    It returns the formatted string, and either one drops the semicolon at
    the end (and gets ans = ...) or surround it with disp(sprintf(...)). I
    introduce the latter, because it looks nicer in the output.

--- handin2/src/knntestrun.m
+++ handin2/src/knntestrun.m
@@ -1,10 +1,10 @@
 function x = knntestrun(train, test, distfunc, Ks)
-  minErr = [2 -1];
-  for k = Ks
-    err = knntest(train, test, distfunc, k);
-    sprintf('%d-Nearest Neighbor error rate: %f', k, err);
-    if (err < minErr(1)) 
-      minErr = [err k];
+    minErr = [2 -1];
+    for k = Ks
+        err = knntest(train, test, distfunc, k);
+        disp(sprintf('%d-Nearest Neighbor error rate: %f', k, err));
+        if (err < minErr(1)) 
+            minErr = [err k];
+        end
     end
-  end
-  x = minErr(2);
+    x = minErr(2);
