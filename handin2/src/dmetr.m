05f85243b7d22b8a998d50265f30b491481e73f1
Philip Pickering <pgpick@gmx.at>
2012-03-01 11:32:58 +0100

    Added comments

--- handin2/src/dmetr.m
+++ handin2/src/dmetr.m
@@ -1,3 +1,10 @@
 function d = dmetr(x, z)
+%% dmetr - calculate metric as given in 3.2
+%% 
+%% x - first value (also called x in assignment)
+%% z - second value (also called z in assignment)
+%%
+%% d = distance between x and z
+
   M = [100 0; 0 1];
   d = norm(M*x - M*z);
