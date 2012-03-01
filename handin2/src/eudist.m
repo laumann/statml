33cb6f3fd683d7494ee4e8a59ce8c6f5083c75a5
Philip Pickering <pgpick@gmx.at>
2012-03-01 11:36:31 +0100

    Added comments and explanation of possible optimization

--- handin2/src/eudist.m
+++ handin2/src/eudist.m
@@ -1,6 +1,19 @@
 function d = eudist(v1, v2)
+%% eudist - calculate euclidean distance
+%%
+%% v1, v2 - inputs
+%%
+%% d = euclidean distance between v1 and v2
+
     m = v1 - v2;
     m = m.^2;
     m = sum(m);
-    m = sqrt(m); % who cares. would be faster without
+
+    %% possible optimization: 
+    %% - omit sqrt. 
+    %% consequence:
+    %% - results are higher, but still comparable
+    %% - faster
+    m = sqrt(m); 
+
     d = m;
