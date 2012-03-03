e7e4195593292de4dde36b479340a3aa64e01a26
Thomas Bracht Laumann Jespersen <laumann.thomas@gmail.com>
2012-03-03 17:23:09 +0100

    Tables of k-NN results and usage of d-metric.
    Introduce tables in section 3 of error rates (probably needs checking).
    Also implement in useknn the usage of d-metric on the knollC data and
    create table in report.

--- handin2/src/useknn.m
+++ handin2/src/useknn.m
@@ -4,19 +4,44 @@ euclid = @(x,y) eudist(x,y);
 Ks = [1 3 5 7 9];
 
 %% knollA
+disp('Performing k-NN on knollA');
+disp('=========================');
 trainA = loadknoll('knollA-train.dt');
 testA  = loadknoll('knollA-test.dt');
 
+disp('Training Set')
 knntestrun(trainA, testA, euclid, Ks)
+disp('Test set')
+knntestrun(trainA, trainA, euclid, [3 5 7 9])
+
 
 %% knollB
+disp('Performing k-NN on knollB');
+disp('=========================');
 trainB = loadknoll('knollB-train.dt');
 testB  = loadknoll('knollB-test.dt');
 
+disp('Training Set')
 knntestrun(trainB, testB, euclid, Ks)
+disp('Test set')
+knntestrun(trainB, trainB, euclid, [3 5 7 9])
+
 
 %% knollC
+disp('Performing k-NN on knollC');
+disp('=========================');
 trainC = loadknoll('knollC-train.dt');
 testC  = loadknoll('knollC-test.dt');
 
+disp('Training Set')
 knntestrun(trainC, testC, euclid, Ks)
+disp('Test set')
+knntestrun(trainC, trainC, euclid, [3 5 7 9])
+
+
+
+%% Using d-metric on knollC
+disp('Performing k-NN on knollC using d-metric');
+disp('========================================');
+knntestrun(trainC, testC, @(x,y) dmetr(x,y), Ks)
+knntestrun(trainC, testC, @(x,y) dmetr(x,y), [3 5 7 9])
