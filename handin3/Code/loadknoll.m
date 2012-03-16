c70d128ead72f5a1da0a7542a970cbd8a107c567
Marco <marco@marco-laptop-private.(none)>
2012-03-16 16:22:52 +0100

    Minor changes on everything, added loadknoll, normalization is optional, division function for bounded and free SVs, slightly different choice for gamma

---
+++ handin3/Code/loadknoll.m
@@ -0,0 +1,13 @@
+function data = loadknoll(file)
+%% Load KNOLL from data file and return the data as a 100x2 matrix
+%% with double values and the its corresponding class in a column
+%% vector of size 100 with integer entries.
+
+%% The reason for returning two matrices instead of one, is because
+%% MATLAB is seriously limited in terms of mixing data types in a
+%% matrix (it cannot). The class vector is of types int32 and our
+%% data is double.
+
+    fid = fopen(file);
+    data = cell2mat(textscan(fid, '%n %n %n', 'Headerlines', 1));
+    fclose(fid);
