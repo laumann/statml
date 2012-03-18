f601ee19199a0ecf1a365a90a8d8eafb3ed3ff4b
Thomas Bracht Laumann Jespersen <laumann.thomas@gmail.com>
2012-03-18 23:01:39 +0100

    A little report writing.
    Introduce tables in report with results from modifying the parameter C
    after model has been selected. Misses the analysis. Introduce another
    table showing the optimal values for C and gamma after model selection
    by grid-search.

--- handin3/Code/runsvm.m
+++ handin3/Code/runsvm.m
@@ -22,17 +22,26 @@ disp('Model selection for knollC-train400');
 %% We now train our SVM on each dataset using the respective values
 %% we found for C and gamma.
 
-c100model=train(knollC100(:,1:2), knollC100(:,3), c100, gamma100);
+c100model=train(knollC100, c100, gamma100);
 
-c200model=train(knollC200(:,1:2), knollC200(:,3), c200, gamma200);
+c200model=train(knollC200, c200, gamma200);
 
-c400model=train(knollC400(:,1:2), knollC400(:,3), c400, gamma400);
+c400model=train(knollC400, c400, gamma400);
 
 %% And run all instances on themselves (and the others?) and the test data
 
 %% TODO
 
 
+
+%% Get number of free and bounded support vectors
+[free100 bounded100] = dividesupportvectors(c100, c100model.SVs, c100model.sv_coef);
+[free200 bounded200] = dividesupportvectors(c200, c200model.SVs, c200model.sv_coef);
+[free400 bounded400] = dividesupportvectors(c400, c400model.SVs, c400model.sv_coef);
+
+disp(sprintf('knollC100: Free SVs: %d Bounded SVs: %d', length(free100), length(bounded100)));
+disp(sprintf('knollC200: Free SVs: %d Bounded SVs: %d', length(free200), length(bounded200)));
+disp(sprintf('knollC400: Free SVs: %d Bounded SVs: %d', length(free400), length(bounded400)));
 
 %% Visualizing the SVM solution
 
@@ -45,11 +54,8 @@ plot(class1(:, 1), class1(:, 2), 'rx');
 hold on;
 plot(class2(:, 1), class2(:, 2), 'bx');
 
-%% Get the free and bounded support vectors
-[free bounded] = dividesupportvectors(c200, c200model.SVs, c200model.sv_coef);
-
-%% And plot them: bounded SVs in green, free ones in black
-plot(bounded(:,1), bounded(:,2), 'go');
-plot(free(:,1), free(:,2), 'ko');
+%% Plot SVs: bounded SVs in green, free ones in black
+plot(bounded200(:,1), bounded200(:,2), 'go');
+plot(free200(:,1), free200(:,2), 'ko');
 
 print -dpsc freeBoundedSVs.eps;
