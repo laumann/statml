c6ede211b80f556d5934664bff5efe1de20a5ff5
Thomas Bracht Laumann Jespersen <laumann.thomas@gmail.com>
2012-03-18 20:57:02 +0100

    Added report to repo, modified SVM model selection.
    The modelselect method now returns the configuration for C and gamma
    that give the best results (using 5-fold cross validation). These are
    then passed to the train method, and the resultant graph seems lighter
    in terms of support vectors (there are _way_ fewer).
    
    runsvm for some reason only passed the entire data set (as one) to the
    train function, whereas it seems to expect the data in two vectors - one
    for data and one for labels. I modified the calls in runsvm to do this.

--- handin3/Code/modelselect.m
+++ handin3/Code/modelselect.m
@@ -1,33 +1,36 @@
-function modelselect (knolldata, normal)
+function [C gamma] = modelselect(knolldata, normal)
 %% Model Selection
 %% For the given data set, we try a number of different values for c and
 %% gamma, perform 5-fold cross validation on each combination of those
 %% values and print the results. 
-%% If requested, normalizes the data before doing the cross-validation.
-    
-    if nargin<2
-        normal=0;
-    end
-    
-    dims=size(knolldata, 2);
-    
-    c=[0.1, 1, 10, 100, 1000, 10000];
-    gamma=[0.0001, 0.001, 0.01, 0.1, 1, 10, 100];
+%% If requested, normalizes the data before doing the
+%% cross-validation.
 
-    labels=knolldata(:, dims);
     
-    data=knolldata(:, 1:(dims-1));
+    if nargin < 2
+        normal = 0;
+    end
+
+    %% Structure of the data is always the same...
+    labels = knolldata(:, 3);
+    data = knolldata(:, 1:2);
     
     if normal
-        data=normalize(data);
+        data = normalize(data);
     end
     
-    for i=1:length(c)
-       for j=1:length(gamma)
-           fprintf('C: %d, gamma: %d.', c(i), gamma(j));
-           crossval(5, data, labels, c(i), gamma(j));
-           
-       end
-    end
+    Cs = [0.1, 1, 10, 100, 1000, 10000];
+    gammas = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100];
 
+    maxp = 0;
+    for c = Cs
+       for g = gammas
+           fprintf('C: %d, gamma: %f ', c, g);
+           p = crossval(5, data, labels, c, g);
+           if maxp < p
+               C = c;
+               gamma = g;
+               maxp = p;
+           end
+       end
     end
