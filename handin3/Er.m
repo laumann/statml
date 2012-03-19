1064e249a3cdff746f772901c0e3dc39fa5c54e0
Philip Pickering <pgpick@gmx.at>
2012-03-20 05:10:49 +0800

    modify Er.m: change length to size for matrix size

--- handin3/Er.m
+++ handin3/Er.m
@@ -1,7 +1,7 @@
 function error = Er(Ws, tdata, acts)
 %singleErrors = [];
     Esum = 0;
-    for i = 1:length(tdata)
+    for i = 1:size(tdata, 1)
         az = runNN(Ws, tdata(i, 1), acts);
         result = az(length(Ws), 2);
         %singleErrors = [singleErrors;(result - tdata(i, 2))^2];
