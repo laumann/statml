7c44c7cdd77b45595346d06dfda460c75e9e7bd4
Philip Pickering <pgpick@gmx.at>
2012-03-19 11:31:55 +0100

    modify nntrain: use maxmax

--- handin3/nntrain.m
+++ handin3/nntrain.m
@@ -11,18 +11,18 @@ actd = @(u) 1/(1 + abs(u))^2;
 %     h
 % end
 
-eps = 0.001
+eps = 0.001;
 
 Ws = initWs(h);
 allowed = initAllowed(Ws);
 acts = initActs(h, act);
 err = 1;
-LR = 0.001
+LR = 0.001;
 
 while eps < err
     accPartDerivs = accPds(Ws, sincTrain50, acts, actd, allowed);
     Ws = modWs(Ws, LR, accPartDerivs);
     numEst = estimatePds(Ws, eps, sincTrain50, acts);
-    err = max(accPartDerivs - numEst);
-    err
+    err = max(max(accPartDerivs - numEst))
+
 end
