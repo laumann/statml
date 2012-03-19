50792860ad01b5935ca72a0a50c2a923489197ba
Philip Pickering <pgpick@gmx.at>
2012-03-19 20:02:29 +0800

    modify estimatePds: optimization by using modifiable-vector instead of iterating through all

--- handin3/estimatePds.m
+++ handin3/estimatePds.m
@@ -1,13 +1,12 @@
-function epds = estimatePds(Ws, eps, tdata, acts)
+function epds = estimatePds(Ws, eps, tdata, acts, chgs)
     size = length(Ws);
     Z = zeros(size, size);
     epds = Z;
     ErTheta = Er(Ws, tdata, acts);
-    for i = 1:size
-        for j = 1:size
-            epse = Z;
-            epse(i, j) = eps;
-            epds(i, j) = (Er(Ws+epse, tdata, acts) - ErTheta)/eps;
-        end
+    
+    for i = chgs
+        Wepse = Ws;
+        Wepse(i) = Wepse(i) + eps;
+        epds(i) = (Er(Wepse, tdata, acts) - ErTheta)/eps;        
     end
     
