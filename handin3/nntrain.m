561349b623d499f91cfab4deb4693912462d2c22
Philip Pickering <pgpick@gmx.at>
2012-03-19 13:06:10 +0100

    modify nntrain to calculate errors between numEstimate of derivs and real derivs

--- handin3/nntrain.m
+++ handin3/nntrain.m
@@ -2,6 +2,7 @@ clear
 
 
 load Data/sincTrain50.dt
+load Data/sincTest50.dt
 h = 20;
 act = @(u) u/(1 + abs(u));
 actd = @(u) 1/(1 + abs(u))^2;
@@ -11,7 +12,7 @@ actd = @(u) 1/(1 + abs(u))^2;
 %     h
 % end
 
-eps = 0.001;
+eps = 0.0001;
 
 Ws = initWsRand(h);
 allowed = initAllowed(Ws);
@@ -19,26 +20,39 @@ acts = initActs(h, act);
 err = 1;
 LR = 0.001;
 
+mods = findMods(allowed);
+
+%%Ws = multiBatchTrain(Ws, sincTrain50, acts, actd, allowed, LR,
+%%500);
+i = 0;
+runs = 500;
 while eps < err
+    Ws = multiBatchTrain(Ws, sincTrain50, acts, actd, allowed, LR, ...
+                        runs);
+    i = i + 1;
     accPartDerivs = accPds(Ws, sincTrain50, acts, actd, allowed);
     Ws = modWs(Ws, LR, accPartDerivs);
-    numEst = estimatePds(Ws, eps, sincTrain50, acts);
-
-
-    err = max(accPartDerivs - numEst);
-    err
+    
+    numEstTest = estimatePds(Ws, eps, sincTest50, acts, mods);
+    diff = abs(accPartDerivs - numEstTest);
+    errTest = mean(diff(mods));
+    
+    numEst = estimatePds(Ws, eps, sincTrain50, acts, mods);
+    diff = (abs(accPartDerivs - numEst));
+    err = mean(diff(mods));
+    [i runs err errTest]
 end
 
 
 %% Use runNN to predict the entire range
 %% nnrange = @(r) arrayfun(runNN(Ws, in acts), r)
 
-getres = @(az) az(length(az), 2)
-nnmodel = @(x) getres(runNN(Ws, x, acts))
+getres = @(az) az(length(az), 2);
+nnmodel = @(x) getres(runNN(Ws, x, acts));
 
 r = -10:.05:10;
-semilogy(r, sin(r)./r); hold on
-semilogy(r, arrayfun(nnmodel, r), 'xr');
+%semilogy(r, sin(r)./r); hold on
+%semilogy(r, arrayfun(nnmodel, r), 'xr');
 
-print -dpsc 'semilognn.eps'
+%print -dpsc 'semilognn.eps'
 
