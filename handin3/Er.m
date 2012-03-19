6acba54cd4de2561bef9c215230c1a68a1205258
Philip Pickering <pgpick@gmx.at>
2012-03-19 20:00:48 +0800

    modify Er.m: optimization

--- handin3/Er.m
+++ handin3/Er.m
@@ -1,9 +1,12 @@
 function error = Er(Ws, tdata, acts)
-    singleErrors = [];
+%singleErrors = [];
+    Esum = 0;
     for i = 1:length(tdata)
         az = runNN(Ws, tdata(i, 1), acts);
         result = az(length(Ws), 2);
-        singleErrors = [singleErrors;(result - tdata(i, 2))^2];
+        %singleErrors = [singleErrors;(result - tdata(i, 2))^2];
+        Esum = Esum + (result - tdata(i, 2))^2;
     end
-    error = (1/2)*sum(singleErrors);
+    %error = (1/2)*sum(singleErrors);
+    error = Esum/2;
     
